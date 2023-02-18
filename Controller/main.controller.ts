fa540c56e85864c828aac7bc950dfdd396695fae
SoRa <paradorn3237@gmail.com>
2023-02-18 10:48:29 +0700

    .

--- Controller/main.controller.ts
+++ Controller/main.controller.ts
@@ -4,12 +4,10 @@ const { ObjectID , ObjectId} = require('bson');
 const model = require('../model/main.model');
 
 import express  from "express";
-type Action = {
-    req: express.Request;
-    res: express.Response;
-}
+type Action =  express.Request;
+type Reaction   =  express.Response;
 
-exports.exampleMethod = ({req,res}:Action) => {
+exports.exampleMethod = (req:Action,res:Reaction) => {
     const User = model
 
     const new_user = new User({
