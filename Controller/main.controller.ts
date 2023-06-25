2a93bbee6cd483becfef854c52b71a55965ee49f
SoRa <paradorn3237@gmail.com>
2023-06-25 12:09:49 +0700

    .

--- Controller/main.controller.ts
+++ Controller/main.controller.ts
@@ -1,8 +1,17 @@
 //This file to make Controller process
 import {Request ,Response} from "express";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
 
 export async function get(req:Request,res:Response){
-    console.log("hello")
+    const new_user = await prisma.user.create({
+        data:{
+            email : "paradorn@gmail.com",
+            name : "dose"
+        }
+    })
+    res.send(new_user)
 }
 
 export default {get};
