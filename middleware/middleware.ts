7340a8e4517fb9c73ed72b978d2713669eaf1bbd
D0NN1E <paradorn3237@gmail.com>
2023-11-30 20:16:46 +0700

    .

--- middleware/middleware.ts
+++ middleware/middleware.ts
@@ -3,7 +3,7 @@ import cors from 'cors';
 import { rateLimit } from 'express-rate-limit';
 
 const app = express();
-  
+
 const limiter = rateLimit({
 	windowMs: 15 * 60 * 1000, // 15 minutes
 	limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
